c3d5cff03720479bc28e4d698db101378681e9e2
mknordgard <56786877+mknordgard@users.noreply.github.com>
2020-02-12 17:29:11 +0100

    Added tb for SmolDrill1

---
+++ SmolDrill1/tb/logic_tb.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+
+entity logic_tb is
+end entity logic_tb;
+
+architecture tb of logic_tb is
+
+  signal IN0        : std_logic;
+  signal IN1        : std_logic;
+  signal IN2        : std_logic;
+  signal IN3        : std_logic;
+  signal OUT0       : std_logic;
+  signal OUT1       : std_logic;
+  signal OUT2       : std_logic;
+
+begin
+
+  process
+  begin
+    -- Test K0
+    IN2 <= '0';  IN1 <= '0';  IN0 <= '0'; 
+    wait for 10 NS;
+    -- Test K1
+    IN2 <= '0';  IN1 <= '0';  IN0 <= '1'; 
+    wait for 10 NS;
+    -- Test K2
+    IN2 <= '0';  IN1 <= '1';  IN0 <= '0'; 
+    wait for 10 NS;
+    -- Test K3
+    IN2 <= '0';  IN1 <= '1';  IN0 <= '1'; 
+    wait for 10 NS;
+    -- Test K4
+    IN2 <= '1';  IN1 <= '0';  IN0 <= '0'; 
+    wait for 10 NS;
+    -- Test K5
+    IN2 <= '1';  IN1 <= '0';  IN0 <= '1'; 
+    wait for 10 NS;
+    -- Test K6
+    IN2 <= '1';  IN1 <= '1';  IN0 <= '0'; 
+    wait for 10 NS;
+    -- Test K7
+    IN2 <= '1';  IN1 <= '1';  IN0 <= '1'; 
+    wait for 10 NS;
+
+    wait;
+  end process;
+
+  M: entity work.logic(RTL)
+  port map (
+    IN0  => IN0,
+    IN1  => IN1,
+    IN2  => IN2,
+    OUT0 => OUT0,
+    OUT1 => OUT1,
+    OUT2 => OUT2
+  );
+
+end architecture tb;
---
+++ common/do_modelsim.tcl
@@ -0,0 +1,79 @@
+puts {
+  ModelSimSE general compile script version 1.0
+  Copyright (c) Doulos June 2004, SD}
+puts "  $this_script_name"
+proc r  {} "uplevel #0 source $this_script_name"
+proc rr {} {global last_compile_time
+            set last_compile_time 0
+            r                            }
+proc q  {} {quit -force                  }
+
+# Prefer a fixed point font for the transcript
+set PrefMain(font) {Courier 10 roman normal}
+
+# Compile out of date files
+set time_now [clock seconds]
+if [catch {set last_compile_time}] {
+  set last_compile_time 0
+}
+foreach {library file_list} $library_file_list {
+  vlib $library
+  vmap work $library
+  foreach file $file_list {
+    set file [subst $file]
+    if { $last_compile_time < [file mtime $file] } {
+      if [regexp -nocase {\.(vhdl|vhd|vho)$} $file] {
+        vcom -2008 $file
+      } else {
+        vlog $file
+      }
+      set last_compile_time 0
+    }
+  }
+}
+set last_compile_time $time_now
+
+# Load the simulation
+regsub -all {\s+} $top_level { } top_level ;# Remove linebreaks
+eval vsim -voptargs=+acc $top_level
+
+# If waves are required
+if [llength $wave_patterns] {
+  noview wave
+  foreach pattern $wave_patterns {
+    add wave $pattern
+  }
+  configure wave -signalnamewidth 1
+  foreach {radix signals} $wave_radices {
+    foreach signal $signals {
+      catch {property wave -radix $radix $signal}
+    }
+  }
+}
+
+# Run the simulation
+run -all
+
+# If waves are required
+if [llength $wave_patterns] {
+  wave zoomfull
+}
+
+puts -nonewline {\
+Commands:  r = Recompile changed and dependent files
+           rr = Recompile everything
+            q = Quit without confirmation}
+
+# How long since project began?
+if {[file isfile start_time.txt] == 0} {
+  set f [open start_time.txt w]
+  puts $f "Start time was [clock seconds]"
+  close $f
+} else {
+  set f [open start_time.txt r]
+  set line [gets $f]
+  close $f
+  regexp {\d+} $line start_time
+  set total_time [expr ([clock seconds]-$start_time)/60]
+  puts "    ($total_time minutes elapsed)"
+}
